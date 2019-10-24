a2f569994f28c751fcb952af33e346d88c873bf8
Đỗ Trung Đức <duc010298@gmail.com>
2019-10-24 21:08:20 +0700

    init project

---
+++ app/src/main/java/com/github/duc010298/musicplayer/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.duc010298.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.github.duc010298.musicplayer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
